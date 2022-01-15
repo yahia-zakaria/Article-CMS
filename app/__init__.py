eed294e4b280d0dc078717bb28f5fbd80b7666ef
yahia-zakaria <yahiazakaria91@hotmail.com>
2022-01-15 19:19:42 +0300

    change to pyodbc anf alias

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager, login_manager
 from flask_login.utils import login_user
-import pyodbc as pyodbc
+import pyodbc 
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
