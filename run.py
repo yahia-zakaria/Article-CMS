b7afedf3390b53b0b3d64743b5aa87424b1126f0
yahia-zakaria <yahiazakaria91@hotmail.com>
2022-01-17 18:22:19 +0300

    202201170622

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from app import app 
 from flask import render_template
+from flask_login import login_required
 
 @app.route('/')
+@login_required
 def index():
     return render_template("base.html")
 
